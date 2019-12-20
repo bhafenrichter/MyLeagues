dc032f2c78f10fa165afea6ffe17ddf15a9b38f2
Brandon Hafenrichter <bhafenri@gmail.com>
2019-12-20 11:45:30 -0600

    Added Plus Button

---
+++ components/Common/PlusButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+import PropTypes from 'prop-types'
+
+const PlusButton = (props) => {
+  const {onPress} = props;
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text style={styles.plus}>+</Text>
+    </TouchableOpacity>
+  )
+}
+
+PlusButton.propTypes = {
+  onPress: PropTypes.func.isRequired,
+}
+
+export default PlusButton
+
+const styles = StyleSheet.create({
+  plus: {
+    fontSize: 42,
+    borderRadius: 36,
+    borderWidth: 1,
+    borderColor: 'black',
+    width: 48,
+    height: 48,
+    textAlign: "center",
+  }
+});
